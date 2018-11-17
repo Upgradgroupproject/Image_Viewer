3e94293d780b7a7ffb6cfa7ec20f1fa402b734ad
pwndubey77 <pwndubey77@gmail.com>
2018-11-17 13:05:14 +0530

    added state and insta xhr requests for profile

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Login from '../screens/login/Login';
+//import Home from '../screens/home/Home';
+import Profile from '../screens/profile/Profile';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -8,15 +10,23 @@ class Controller extends Component {
 
   constructor() {
     super();
-    this.baseUrl = "https://api.instagram.com/v1";
+    this.baseUrl = "127.0.0.1:3000"; //http://localhost:3000
   }
   render() {
     return (
       <Router>
         <div className="main-container">
           <Route exact path='/' render={(props) => <Login {...props} baseUrl={this.baseUrl} />} />
-          <Route exact path='/Login' render={(props) => <Login {...props} baseUrl={this.baseUrl} />} />
+          <Route exact path='/login' render={(props) => <Login {...props} baseUrl={this.baseUrl} />} />
           {/* <Route exact path='/Home' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} /> */}
+
+          {/* <Route path='/home' render={ (props) => (
+            sessionStorage.getItem("access-token") === null? <Login  {...props}/> : <Home {...props} />
+          )} baseUrl={this.baseUrl} /> */}
+
+          <Route path='/profile' render={ (props) => (
+            sessionStorage.getItem("access-token") === null? <Login  {...props}/> : <Profile {...props} />
+          )} baseUrl={this.baseUrl} />
           
 
         </div>
